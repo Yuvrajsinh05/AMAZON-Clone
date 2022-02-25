460e414f4e9be692666a800e2fa73198692d7b9d
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-02-26 00:20:15 +0530

    AMAZON

---
+++ script1.js
@@ -0,0 +1,11 @@
+
+function open() {
+  document.getElementById("aside").style.width = "100%";
+  document.getElementById("aside").style.display = "block";
+}
+
+function close() {
+  document.getElementById("aside").style.display = "none";
+}
+
+     
