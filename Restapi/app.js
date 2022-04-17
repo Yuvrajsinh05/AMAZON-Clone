dd559f0b0c184d3106d2dfb92b034c5cf04aa787
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-04-17 14:19:49 +0530

    api

---
+++ Restapi/app.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let app = express();
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT || 8230;
+const mongoUrl = process.env.mongoUrl
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hey Welcome to the amazonC</h1>')
+})
+
+app.get('/Data',(req,res)=>{
+    db.collection('Data').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+
+}
+)
+// Connection with db
+MongoClient.connect(mongoUrl, (err,client) => {
+    if(err) console.log(`Error while connecting`,err);
+    db = client.db("amazonC");
+    app.listen(port,() => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
