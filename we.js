26d592a94b36a353760442465c54a941f3ca927f
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-05-11 17:43:01 +0530

    JSON

--- we.js
+++ we.js
@@ -1,5 +1,6 @@
 var x = document.getElementById("out");
 var y = document.getElementById("weatherOut");
+var z = document.getElementById("")
 function geolocation(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition)
@@ -10,20 +11,22 @@ function geolocation(){
 
 function showPosition(data){
     console.log(data)
-    x.innerText=`Latitude is ${data.coords.latitude}, longitude is ${data.coords.longitude}`
+    // x.innerText=`Latitude is ${data.coords.latitude}, longitude is ${data.coords.longitude}`
     let latitude = data.coords.latitude;
     let longitude = data.coords.longitude
     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
     // api calling
     fetch(url,{method: 'GET'})
     //return promise
-    .then((res) => res.json())
-    //return datatype
+    .then((res) => res.json())  
+      //return datatype
     .then((data) => {
         let cityName = data.city.name
-        let weather = `${data.list[0].temp.day} degree`
-        let output = `Weather of ${cityName} is ${weather}`
+        let weather = `${data.list[0].temp.day}C`
+        let output = `${cityName}(${weather})`
         console.log(data)
         y.innerText = output;
     })
+    // document.getElementsByClassName("nloc").innerText =
+    document.getElementById("weather").innerHTML= `${cityName}(${weather})`
 }
